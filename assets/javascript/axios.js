a3e9c7b99fd11775884c0cea687e4cb036716f7c
unknown <t.bakeer4@gmail.com>
2018-05-20 18:23:20 -0700

    added on click function to search button and added ajax request and got the response, put the response to html

--- assets/javascript/axios.js
+++ assets/javascript/axios.js
@@ -1,21 +1,43 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
- 'api-key': "4ce897641c3446babede737c831ec9ad",
- 'q': "roosevelt",
- 'begin_date': "20080721",
- 'end_date': "20180820"
-});
 
+var q;
+// var beginDate="19900213";
+// var endDate="20180101";
+var apiKey="4ce897641c3446babede737c831ec9ad";
+var numRecords;
 
-
-        $.ajax({
-            url: url,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-                console.log(JSON.stringify(response.response.docs));
+           $("#search-button").on("click", function(event) {
+                console.log("hhhhhh");
+                event.preventDefault();
+                q= $("#search-term").val().trim();
+                beginDate=$("#start-year").val();
+              endDate=$("#end-year").val();
+               numRecords=$("#num-records").val().trim();
+                console.log(q);
+                var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=4ce897641c3446babede737c831ec9ad&q="+q+"&begin_date="+beginDate+"&end_date="+endDate;
+                $.ajax({
+                    url: url,
+                    method: "GET"
+                })
+                    .then(function (response) {
+                       // console.log(response);
+                       var results=response.response.docs;
+                      for(var i=0;i<numRecords;i++)
+                      {    var p1=$("<p class='p-3'>").text(JSON.stringify(response.response.docs[i].headline.print_headline));
+                           var p2= $("<p class='p-3'>").text(JSON.stringify(response.response.docs[i].web_url));
+                           var div=$("<div class='data '>");
+                           div.append([p1,p2]);
+                           $("#data").prepend(div);
+                       }
+                       console.log(response.response.docs);
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+        
+                  
             })
-            .catch(function (error) {
-                console.error(error);
-            });
+
+
+
+          
+
